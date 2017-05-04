40372f35aaede44182834cafaff4f368c5d3fbbc
hubiqing01 <hubiqing01@58ganji.com>
2017-05-04 11:36:37 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+#  模块名称
+## 描述
+1. 简介1
+2. 简介2
+
+![my love](./logo.png)
+
+### test 测试  
+- npm i -g mocha     
+- yarn 
+- npm run test    
---
+++ test/index.test.ts
@@ -0,0 +1,23 @@
+import * as requireHelper from 'require-helper'
+import { expect } from 'chai';
+import { add } from '../ts/index'
+/**
+ * 同步测试
+ */
+describe('test add', () => {
+    it('1 + 1 should be equal to 2', () => {
+        expect(add(1, 1)).to.equal(2);
+    })
+})
+
+/**
+ * 异步测试
+ */
+describe('test async add', () => {
+    it('2 + 2 should be equal to 4', (done) => {
+        setTimeout(() => {
+            expect(add(2, 2)).to.equal(4);
+            done()
+        }, 2000)
+    })
+})
